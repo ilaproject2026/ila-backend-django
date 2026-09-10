f6c0d91fd4faf1a2d7a6d8c068fa72400e440e7c
Mridhul2k03 <mridhulkrishna11@gmail.com>
2026-09-10 12:04:14 +0530

    feat: configure Django application for Vercel deployment via index.py and vercel.json

---
+++ api/index.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, BASE_DIR)
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "config.settings"
+)
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
